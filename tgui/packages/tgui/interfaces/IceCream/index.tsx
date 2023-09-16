a182b5b7a4363966451db39614a5a74ebb9ae95e
DisturbHerb <66253095+DisturbHerb@users.noreply.github.com>
2023-09-17 00:43:24 +1000

    fix reagent container section

--- tgui/packages/tgui/interfaces/IceCream/index.tsx
+++ tgui/packages/tgui/interfaces/IceCream/index.tsx
@@ -46,11 +46,11 @@ export const IceCream = (_, context) => {
 };
 
 const Flavor = (props: FlavorArray, context) => {
-  const { act } = useBackend<IceCreamData>(context);
+  const { act, data } = useBackend<IceCreamData>(context);
 
   return (
     <Stack.Item width="50%">
-      <Button fluid onClick={() => act('dispense_flavor', { flavor: props.flavorName })}>
+      <Button fluid disabled={!data.cone} onClick={() => act('dispense_flavor', { flavor: props.flavorName })}>
         <Icon
           color={'rgba(' + props.flavorColor + ', 1)'}
           name="circle"
@@ -70,51 +70,54 @@ const InsertedContainer = (_, context) => {
   const containerData = data.containerData ?? NoContainer;
 
   return (
-    <Section
-      capitalize
-      fill
-      title={!data.containerData ? containerData.name : ""}
-      buttons={
-        <Stack fluid>
+    <Section fill>
+      <Stack fill vertical>
+        <Stack.Item>
           {!!data.containerData && (
-            <Stack.Item>
-              <Button onClick={() => act('dispense_flavor', { flavor: 'beaker' })}>
-                <Icon
-                  color={containerData.finalColor}
-                  name="circle"
-                  pt={1}
-                  style={{
-                    'text-shadow': '0 0 3px #000',
-                  }}
-                />
-                {capitalize(containerData.name)}
-              </Button>
-            </Stack.Item>
+            <Stack align="end" justify="space-between">
+              <Stack.Item>
+                <Button
+                  onClick={() => act('dispense_flavor', { flavor: 'beaker' })}
+                  style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}>
+                  <Icon
+                    color={containerData.finalColor}
+                    name="circle"
+                    pt={1}
+                    style={{
+                      'text-shadow': '0 0 3px #000',
+                    }}
+                  />
+                  {capitalize(containerData.name)}
+                </Button>
+              </Stack.Item>
+              <Stack.Item align="end">
+                <Button onClick={() => act('eject_beaker')} icon="eject" disabled={!data.containerData}>
+                  Eject
+                </Button>
+              </Stack.Item>
+            </Stack>
           )}
-          <Stack.Item>
-            <Button onClick={() => act('eject_beaker')} icon="eject" disabled={!data.containerData}>
-              Eject
-            </Button>
-          </Stack.Item>
-        </Stack>
-      }>
-      <ReagentGraph container={containerData} />
-      <ReagentList container={containerData} />
-      <Box className="ChemHeater__TemperatureBox" textAlign="center">
-        {!!containerData.totalVolume && (
-          <Box fontSize={2} className={'ChemHeater__TemperatureNumber'}>
-            <Icon name={getTemperatureIcon(containerData.temperature)} pr={0.25} />
-            <AnimatedNumber value={containerData.temperature} /> K
+        </Stack.Item>
+        <Stack.Item grow>
+          <ReagentGraph container={containerData} />
+          <ReagentList container={containerData} />
+          <Box className="ChemHeater__TemperatureBox" textAlign="center">
+            {!!containerData.totalVolume && (
+              <Box fontSize={2} className={'ChemHeater__TemperatureNumber'}>
+                <Icon name={getTemperatureIcon(containerData.temperature)} pr={0.25} />
+                <AnimatedNumber value={containerData.temperature} /> K
+              </Box>
+            )}
           </Box>
-        )}
-      </Box>
-      {!data.containerData && (
-        <Dimmer>
-          <Button icon="eject" fontSize={1.5} onClick={() => act('insert_beaker')} bold>
-            Insert Container
-          </Button>
-        </Dimmer>
-      )}
+          {!data.containerData && (
+            <Dimmer>
+              <Button icon="eject" fontSize={1.5} onClick={() => act('insert_beaker')} bold>
+                Insert Container
+              </Button>
+            </Dimmer>
+          )}
+        </Stack.Item>
+      </Stack>
     </Section>
   );
 };
